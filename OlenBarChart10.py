8b191472b263f9980448b006c303c3eb53703489
abessen <abessen@quarryvision.com>
2023-05-13 14:10:28 -0500

    new commit

--- OlenBarChart10.py
+++ OlenBarChart10.py
@@ -30,7 +30,7 @@ def add_logo(logo_path, width, height):
     modified_logo = logo.resize((width, height))
     return modified_logo
 
-my_logo = add_logo(logo_path="Alan.jpg", width=30, height=40)
+my_logo = add_logo(logo_path="Alan.jpg", width=40, height=40)
 
 if my_logo is not None:
     # Use st.image to display the logo in the main display
@@ -38,7 +38,7 @@ if my_logo is not None:
 
 # List all files in the current directory
 ##files = os.listdir()
-st.write(files)
+#st.write(files)
 
 
 
